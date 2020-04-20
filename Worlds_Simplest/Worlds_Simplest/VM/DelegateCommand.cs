7716af9ce76dbc722c39a86eb09c6ec7f93489da
Arjun <arjunpr@gmil.com>
2020-04-20 16:22:23 +0100

    Commit with the First example ('World's simplest MVVM')

---
+++ Worlds_Simplest/Worlds_Simplest/VM/DelegateCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Worlds_Simplest.VM
+{
+    public class DelegateCommand : ICommand
+    {
+        private readonly Action _action;
+#pragma warning disable 67
+        public event EventHandler CanExecuteChanged;
+#pragma warning restore 67
+
+        public DelegateCommand(Action action)
+        {
+            _action = action;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _action();
+        }
+    }
+}
